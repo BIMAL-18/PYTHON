d538998ec2b87fea204b2ab163703e79ab8af100
BIMAL-18 <bimalknwr3@gmail.com>
2024-08-20 09:51:53 +0545

    Add files via upload

---
+++ Shortcut trick/shortcut.py
@@ -0,0 +1,4 @@
+# To change the same in a all program we User
+# select the name
+# double click on it 
+# and click ctrl+ f2 or double right click and click on change all occourence and we change it 
