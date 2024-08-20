881a3f4a996e51e610790a62b674c4dbb3b16ec5
BIMAL-18 <bimalknwr3@gmail.com>
2024-08-20 09:45:36 +0545

    Add files via upload

---
+++ Chapter 5 Practice set/nepali_to_english_trans.py
@@ -0,0 +1,10 @@
+# write a program to craete a dictionary of Nepali words with Values as their English translations .
+# Provide user with an option to look it up
+
+word = {
+    "Kukur": "Dog",
+    "Biralo":"Cat",
+    "Gadha" : "Aayush"
+}
+words =input("Enter the word that you want to get meaning : ")
+print(word[words])
